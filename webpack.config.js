4188c9f1416ac4be7e588d1b30f20cdf2a9c3aa9
Vladimir Zemskov <zemskov.va@esphere.ru>
2021-01-03 20:14:09 +0300

    поправлен дев сервер

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,41 @@ const webpack = require('webpack');
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const config = {
-  entry: './src/index.ts',
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.ts'
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    filename: '[name].[contenthash].js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
-          },
-          'postcss-loader',
-        ],
+          'css-loader'
+        ]
       },
       {
         test: /\.ts(x)?$/,
         loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
   },
   plugins: [
     new CopyPlugin({
@@ -46,17 +44,20 @@ const config = {
     }),
     new HtmlWebpackPlugin({
       appMountId: 'app',
-      filename: 'index.html',
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      openAnalyzer: false,
+      filename: 'index.html'
     }),
     new MiniCssExtractPlugin(),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ],
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
   },
   optimization: {
     runtimeChunk: 'single',
@@ -65,11 +66,18 @@ const config = {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
-  },
+          chunks: 'all'
+        }
+      }
+    }
+  }
 };
 
-module.exports = config;
+module.exports = (env, argv) => {
+  if (argv.hot) {
+    // Cannot use 'contenthash' when hot reloading is enabled.
+    config.output.filename = '[name].[hash].js';
+  }
+
+  return config;
+};
